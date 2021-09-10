04572bc5d9d3b58d0fb399c9e990e28d4a06a338
Валентина <15valentinka@gmail.com>
2021-09-10 13:21:02 +0500

    Add check in DragDropTest

--- src/test/java/github/DragDropCheck.java
+++ src/test/java/github/DragDropCheck.java
@@ -3,14 +3,18 @@ package github;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.*;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class DragDropCheck {
 
-   @Test
-    void dragDrop(){
+    @Test
+    void dragDrop() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         $("#column-a").dragAndDropTo("#column-b");
+        $("#column-a").shouldHave(text("B"));
+        $("#column-b").shouldHave(text("A"));
+
 
     }
 }
